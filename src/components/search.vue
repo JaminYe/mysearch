f2c016c4290299079cbf58ac53f1d10f42833fed
jaminye <JaminYe2018@outlook.com>
2022-04-06 22:31:02 +0800

    初始化

---
+++ src/components/search.vue
@@ -0,0 +1,30 @@
+<style>
+    .el-input{
+        position: absolute;
+        width:100px;
+        height:50px;
+        top:0;
+        right:0;
+        bottom:0;
+        left:0;
+        margin:auto;
+        background:#f60;
+    }
+</style>
+<template>
+    <div style="width: 100%;height: 100%">
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data() {
+            return {
+                input: ''
+            }
+        }
+    }
+</script>
+
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+    lintOnSave:false,
+    devServer: {
+        proxy: {
+            '/baidu': {
+                target: 'https://www.baidu.com',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/baidi': ''
+                }
+            }
+        }
+    }
+
+
+}
